0d7c10b18855c578c29dcec674d26c17451286c3
rchlkm <rhkim@andrew.cmu.edu>
2017-05-03 04:20:28 -0400

    added content to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+	def new
+    @user = User.new
+	end
+
+	def edit
+	end
+
+	def create
+		@user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to home_path, notice: "Thank you for signing up!"
+    else
+      flash[:error] = "This user could not be created."
+      render "new"
+    end
+  end
+
+	def update
+		if @user.update_attributes(user_params)
+      flash[:notice] = "#{@user.proper_name} is updated."
+      redirect_to @user
+    else
+      render :action => 'edit'
+    end
+	end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :band_id, :password, :password_confirmation, :role, :active)        
+    end
 end
