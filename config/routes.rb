0612921d06fb627c9cc8a6d1ce0e853e17c6798e
rchlkm <rhkim@andrew.cmu.edu>
2017-05-03 04:10:32 -0400

    created proper login routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,21 @@ Rails.application.routes.draw do
   resources :items
   resources :purchases
   resources :item_prices
+  resources :sessions
 
   # Semi-static page routes
   get 'home' => 'home#home', as: :home
   get 'about' => 'home#about', as: :about
   get 'contact' => 'home#contact', as: :contact
   get 'privacy' => 'home#privacy', as: :privacy
+
+  # get 'user/edit' => 'users#edit', :as => :edit_current_user
+  # get 'signup' => 'users#new', :as => :signup
+  get 'login' => 'sessions#new', :as => :login
+  get 'logout' => 'sessions#destroy', :as => :logout
   
-  # Set the root url
+  # Set the root url (default route)
   root :to => 'home#home'  
+  # root :to => 'bands#index', :as => :home
 
 end
