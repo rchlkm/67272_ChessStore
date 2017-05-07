21539c94630734682fc80dacb8b50d5fe4c5c1cd
rchlkm <rhkim@andrew.cmu.edu>
2017-05-07 19:21:11 -0400

    generated order items and order controllers

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,11 @@
 class CartsController < ApplicationController
-end
+	def show
+	 @cart = current_cart
+	end
+
+	def add_to_cart
+	   # current_cart.add_item(params[:item_id])
+	   # redirect_to carts_path(current_cart.id)
+	end
+
+ end
