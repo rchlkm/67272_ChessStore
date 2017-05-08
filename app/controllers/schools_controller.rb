26f05d3ec6814f10788e2e960b826572e75910e3
rchlkm <rhkim@andrew.cmu.edu>
2017-05-08 19:56:32 -0400

    created school controller

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,34 @@
+class SchoolsController < ApplicationController
+  before_action :set_school, only: [:index, :show, :edit, :update, :destroy]
+
+  def index
+  	@schools = School.active.alphabetical.to_a
+  	@inactive_schools = School.inactive.alphabetical.to_a
+  end
+
+  def create
+    @school = School.new(school_params)
+    if @school.save
+      redirect_to :back
+      # redirect_to item_path(@item), notice: "Successfully created #{@item.name}."
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+	
+	private
+	def set_school
+    @school = School.find(params[:id])
+  end
+
+  def school_params
+    params.require(:school).permit(:name, :street_1, :street_2, :city, :state, :zip, :min_grade, :max_grade)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,7 @@ Rails.application.routes.draw do
   # get 'show_cart' => 'carts#show_cart', as: :show_cart
   get 'cart' => 'carts#show', as: :cart
   get 'checkout' => 'orders#new', as: :checkout
+  get 'new_school' => 'schools#new', as: :create_new_school
   
   # Set the root url (default route)
   root :to => 'home#home'
