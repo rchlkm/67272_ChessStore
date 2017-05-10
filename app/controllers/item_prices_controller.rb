72b4df53ba68a9ec059d49b178435444c3cdac5c
rchlkm <rhkim@andrew.cmu.edu>
2017-05-09 21:17:06 -0400

    AJAX WORKS

--- app/controllers/item_prices_controller.rb
+++ app/controllers/item_prices_controller.rb
@@ -16,6 +16,22 @@ class ItemPricesController < ApplicationController
     else
       render action: 'new'
     end
+
+    respond_to do |format|
+      if @item_price.save
+        format.html { redirect_to @item_price, notice: 'Attraction was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @item_price }
+        @item = @item_price.item
+        @item_prices = @item.item_prices.wholesale.chronological.to_a
+        # @town = @attraction.town
+        # @attractions = @town.attractions.alphabetical.to_a
+        format.js
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @item_price.errors, status: :unprocessable_entity }
+      end
+    end
+
   end
 
   private
